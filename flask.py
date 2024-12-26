83ecc8b5544d13431104881f5d4325044fd7b251
qayyum22 <qayyumabdul660@gmail.com>
2024-12-26 14:14:06 +0530

    flask

---
+++ flask.py
@@ -0,0 +1,13 @@
+# https://x.com/i/grok/share/P0FBMzN7VMhW52yzsxLejgi1a
+# Python program to use 
+# main for function call.
+import myscript
+
+def my_function():
+	# Add your function code here
+	pass
+
+if __name__ == "__main__":
+	my_function()
+
+myscript.my_function()
